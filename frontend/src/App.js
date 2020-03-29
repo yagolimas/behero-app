6074853c112b058c22f194c86ac6e7d50ab3998d
Yago Schuenck de Lima <yago.lima@radixeng.com.br>
2020-03-29 15:01:52 -0300

    Removing unnecessary files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p> To be Hero </p>
-      </header>
-    </div>
+    <h1> Be the Hero </h1>
   );
 }
 
