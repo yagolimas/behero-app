6eff3c4c9a3c19de4929fda7f04f73d617bbf4cf
Yago Schuenck de Lima <yago.lima@radixeng.com.br>
2020-03-29 19:19:17 -0300

    Creating Profile screen

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={ logoImg } alt="Be The Hero" />
+                <span>Bem vindo, APAD</span>
+
+                <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+                <button type="button">
+                    <FiPower size={18} color="#E02041"/>
+                </button>
+            </header>
+            <h1>Casos Cadastrados</h1>
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso Teste</p>
+                    <strong>DESCRICAO:</strong>
+                    <p>Descricao Teste</p>
+                    <strong>VALOR:</strong>
+                    <p>100</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+            </ul>
+        </div>
+    );
+}
