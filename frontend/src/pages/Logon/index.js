d43fccafced71aad64c210685f8df74a1aa748b4
Yago Schuenck de Lima <yago.lima@radixeng.com.br>
2020-03-29 18:49:36 -0300

    Creating Register screen

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 
 import './styles.css';
 
@@ -15,10 +16,10 @@ export default function Logon() {
                     <h1>Faca seu logon</h1>
                     <input type="text" placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041"/>
                         Nao encontrado
-                    </a>
+                    </Link>
                 </form>
             </section>
             <img src={ heroesImg } alt="Heroes" />
