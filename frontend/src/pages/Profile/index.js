fa4c5f3f7486885b50726522b2bf3b8bb45bce5d
Yago Schuenck de Lima <yago.lima@radixeng.com.br>
2020-03-30 12:22:36 -0300

    Implements Logout

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import api from '../../services/api';
 
@@ -13,6 +13,8 @@ export default function Profile() {
     const ongId = localStorage.getItem('ongId');
 
     const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
 
     useEffect(() => {
         api.get('profile/', {
@@ -38,6 +40,12 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -45,7 +53,7 @@ export default function Profile() {
                 <span>Bem vindo, { ongName } </span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={ handleLogout } type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
